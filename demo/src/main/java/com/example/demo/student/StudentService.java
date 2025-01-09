c7ab16d39dc3569dcdcb474bb07a3348f49c3779
Sean Kembo <kembosean7@gmail.com>
2025-01-10 01:04:56 +0200

    Create StudentRepository interface for CRUD operations on Student entity

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +10,16 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private  final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Autowired
     public List<Student> getStudents(){
-        return  List.of(
-                new Student(
-                        19L,
-                        "Sean",
-                        "kembosean7@gmail.com",
-                        LocalDate.of(2004, Month.APRIL, 5), 21
-                )
-        );
+        return studentRepository.findAll();
+
     }
 }
