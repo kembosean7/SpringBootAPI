a8d162bb1905db0f0285c301978370bae9e823ae
Sean Kembo <kembosean7@gmail.com>
2025-01-10 01:28:24 +0200

    adding intial student configuration with sample data

--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,4 +1,35 @@
 package com.example.demo.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+             Student sean = new Student(
+                     "Sean",
+                     "kembosean72004@gmail.com",
+                     LocalDate.of(2004, Month.APRIL,5),
+                     21
+             );
+
+             Student mandisi = new Student(
+                     "Mandisi",
+                     "mandisitaro9720@gmail.com",
+                     LocalDate.of(200, Month.AUGUST,23),
+                     24
+             );
+             repository.saveAll(
+                     List.of(sean, mandisi)
+             );
+        };
+    }
 }
